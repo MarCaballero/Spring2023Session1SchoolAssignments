d58e67e53018f53d7879936f416b28b42e80d131
Maria <65955086+MarCaballero@users.noreply.github.com>
2023-02-03 18:15:55 -0500

    Added files via upload
    These are my Assignments for Week#1 Class: CMIS-141

---
+++ SpringSemester2023Session1Assignments/Spring2023SchoolAssignmentsOne/Spring2023SchoolAssignmentsOne/A1.java
@@ -0,0 +1,10 @@
+
+public class A1 {
+//Remember that every program must have a "public static void main" to be able to run
+	public static void main(String[] args) {
+
+		System.out.println("Hello there!");
+
+	}
+
+}
